fd6275847b5b71dc568dda803b817e66eba20bb3
JamesTheCoolDude <83745086+JamesTheCoolDude@users.noreply.github.com>
2021-09-09 15:30:29 -0500

    Update App.js

--- App.js
+++ App.js
@@ -56,6 +56,9 @@ function previewFile() {
     reader.readAsDataURL(file);
   }
 }
+setInterval(() => {
+	previewFile();
+}, 1000)
 (function() {
         var pubnub = new PubNub({
             publishKey: 'demo',
